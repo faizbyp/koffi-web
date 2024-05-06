67842a30cb1a21cbd428d2388e9b7cf62df5714f
faizbyp <faizbyp@gmail.com>
2024-05-06 22:44:44 +0700

    vercel config

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.listen(8080);
+app.listen(5000);
 
 app.get('/', (req, res) => {
   res.render('index');
 })
 
 app.use('/public', express.static('public'));
+
+module.exports = app;
